9fa2c9c254b9744a4120872cf3195ecb987e1712
jaeseok an <k1462487@kcl.ac.uk>
2015-10-17 19:08:24 +0100

    17.10.2015 problem 16 solved but need to review again about bigInteger

--- Problem16.java
+++ Problem16.java
@@ -1,26 +1,34 @@
 package projectEuler;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 //Problem 16
 //215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 //What is the sum of the digits of the number 21000?
+
+
+
 
 public class Problem16 {
 
 	public static void main(String[] args) {
 	
     BigInteger a = new BigInteger("2").pow(1000);	
-    a.toByteArray();
+    BigInteger sum = BigInteger.valueOf(0);
+    BigInteger add = BigInteger.valueOf(0);
+    BigInteger div = BigInteger.valueOf(10);
     
-   
-    for(int i=0; i<a.bitLength(); i++){
-        	System.out.println(a.toString().charAt(i));
-        }
+    String num = String.valueOf(a);
     
-			
+    for(int i=0; i<num.length(); i++){
+    	add=a.remainder(div);
+    	sum=sum.add(add);
+    	a=a.divide(div);
+    }
+  	System.out.println(sum);
 	}
-
 }
+
+//Big Integer is two's-complement notation
+//Why it is stored as two's-complement notation? 
